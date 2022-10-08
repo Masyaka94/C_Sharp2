a5674139bf8e1441c4fb946a69a15210d03dfdae
masyaka.94 <masyaka.94@gmail.com>
2022-10-08 13:28:11 +0500

    dobavlen vtoroi sposob resheniya zadachi

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -3,3 +3,7 @@
 Console.WriteLine ("Vvedite trehznachnoe chislo a");
 string a = (Console.ReadLine());
 Console.WriteLine(a[1]);
+
+// второй способ
+int b = int.Parse(a);
+Console.WriteLine((b %100)/10);
