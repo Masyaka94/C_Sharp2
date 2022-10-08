2048671550b25e726008987db8ad4b319a3f5aea
masyaka.94 <masyaka.94@gmail.com>
2022-10-08 13:16:44 +0500

    добавлен файл гитигнор и задача 10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,5 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.*/
+Console.WriteLine ("Vvedite trehznachnoe chislo a");
+string a = (Console.ReadLine());
+Console.WriteLine(a[1]);
