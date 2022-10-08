3baf54d7c1794cab472fbcd385c70a2f51dcb55d
masyaka.94 <masyaka.94@gmail.com>
2022-10-08 14:10:24 +0500

    Добавлена задача 15

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.*/
+Console.WriteLine("Vvedite nomer dnya nedeli");
+int nedeli =int.Parse(Console.ReadLine());
+if (nedeli > 5 && nedeli <= 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else if (nedeli <= 5)
+{
+        Console.WriteLine("Будний день");
+}
+else 
+{
+    Console.WriteLine("Ошибка ввода");
+}
