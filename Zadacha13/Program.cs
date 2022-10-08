2982c93c2e6f71e4b8132898710ee4c49ec12be8
masyaka.94 <masyaka.94@gmail.com>
2022-10-08 13:55:35 +0500

    добавлена задача 13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Vvedite trehznachnoe chislo a");
+string a = (Console.ReadLine());
+int l = a.Length;
+if (l >= 3)
+{
+    Console.WriteLine(a[2]);
+}
+else
+{
+    Console.WriteLine("Третьего числа нет");
+}
+
+
+// второй способ
+int b = int.Parse(a);
+
+if (b / 100 > 0)
+{
+    Console.WriteLine((b % 100) % 10);
+}
+else
+{
+    Console.WriteLine("Третьего числа нет");
+}
